ae0cb3d36fbc25d569d39182225e9dba71a328f7
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-04-11 08:51:03 +0200

    create checkout page class and test cases

---
+++ src/main/java/CheckoutPage.java
@@ -0,0 +1,72 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPage extends BasePage {
+
+    @FindBy(id = "checkout")
+    private WebElement checkoutButton;
+
+    @FindBy(id = "first-name")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "last-name")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "postal-code")
+    private WebElement postalCodeInput;
+
+    @FindBy(id = "continue")
+    private WebElement continueButton;
+
+    @FindBy(id = "finish")
+    private WebElement finishButton;
+
+    @FindBy(id = "checkout_complete_container")
+    private WebElement checkoutMessage;
+
+    @FindBy(xpath = "//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]")
+    private WebElement informationErrorMessage;
+
+    private void fillFirstName(String firstName) {
+        firstNameInput.sendKeys(firstName);
+    }
+
+    private void fillLastName(String lastName) {
+        lastNameInput.sendKeys(lastName);
+    }
+
+    private void fillPostalCode(int postalCode) {
+        postalCodeInput.sendKeys(String.valueOf(postalCode));
+    }
+
+    public CheckoutPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void clickOnCheckout() {
+        checkoutButton.click();
+    }
+
+    public void fillInDataForCheckout(String firstName, String lastName, int postalCode) {
+        fillFirstName(firstName);
+        fillLastName(lastName);
+        fillPostalCode(postalCode);
+    }
+
+    public void clickOnContinueButton() {
+        continueButton.click();
+    }
+
+    public void clickOnFinishButton() {
+        finishButton.click();
+    }
+
+    public boolean isCheckoutMessageDisplayed() {
+        return checkoutMessage.isDisplayed();
+    }
+
+    public boolean isErrorMessageDisplayed(){
+        return informationErrorMessage.getText().contains("Error");
+    }
+}
---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutTest {
+    private WebDriver webDriver;
+
+    @BeforeEach
+    public void setup() {
+        webDriver = WebDriverProvider.setupWebDriver();
+        webDriver.navigate().to(LoginPage.LOGIN_URL);
+    }
+
+    @Test
+    public void successfulCheckOutTest() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.logIn("standard_user", "secret_sauce");
+
+        MainPage shop = new MainPage(webDriver);
+        CheckoutPage checkout = new CheckoutPage(webDriver);
+
+        shop.addOneElementToCart();
+        shop.navigateToCart();
+
+        checkout.clickOnCheckout();
+        checkout.fillInDataForCheckout("Test", "Test", 1234);
+        checkout.clickOnContinueButton();
+        checkout.clickOnFinishButton();
+
+        Assertions.assertTrue(checkout.isCheckoutMessageDisplayed());
+    }
+
+    @Test
+    public void unsuccessfulCheckOutTest() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.logIn("problem_user", "secret_sauce");
+
+        MainPage shop = new MainPage(webDriver);
+        CheckoutPage checkout = new CheckoutPage(webDriver);
+
+        shop.addOneElementToCart();
+        shop.navigateToCart();
+
+        checkout.clickOnCheckout();
+        checkout.fillInDataForCheckout("Test", "Test", 1234);
+        checkout.clickOnContinueButton();
+
+        Assertions.assertTrue(checkout.isErrorMessageDisplayed());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.err.println("Thread sleep interrupted: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        } finally {
+            webDriver.quit();
+        }
+    }
+}
