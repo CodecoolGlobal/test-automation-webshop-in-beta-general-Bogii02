bccd57f77ac7016038435e288b90578d3723cff6
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-04-11 09:06:41 +0200

    add product details test

---
+++ src/test/java/ProductsDetailsTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class ProductsDetailsTest {
+
+    private WebDriver webDriver;
+
+
+    @BeforeEach
+    public void setup() {
+        webDriver = WebDriverProvider.setupWebDriver();
+        webDriver.navigate().to(LoginPage.LOGIN_URL);
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.logIn("standard_user", "secret_sauce");
+    }
+
+    @Test
+    public void productsDetailsTest() {
+        MainPage mainPage = new MainPage(webDriver);
+        mainPage.checkItemDetails();
+        Assertions.assertTrue(mainPage.areWeOnProductsDetails());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.err.println("Thread sleep interrupted: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        } finally {
+            webDriver.quit();
+        }
+    }
+}
