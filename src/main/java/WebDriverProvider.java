92edfd0ca40ec47c82c2b7fff50e9d636bdb59f1
Fekete Imre <feketeimi12@gmail.com>
2024-04-12 10:41:20 +0200

    modify WebDriverProvider, maven.yml modify WebDriverProvider so it creates a webdriver based on the environment variable BROWSER, also modify maven.yml so it runs the tests with all the browsers that have headless mode

--- src/main/java/WebDriverProvider.java
+++ src/main/java/WebDriverProvider.java
@@ -2,12 +2,38 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class WebDriverProvider {
 
     public static WebDriver setupWebDriver() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        return new ChromeDriver(chromeOptions);
+        String browserName = System.getenv().getOrDefault("BROWSER", "chrome");
+        WebDriver driver;
+
+        switch (browserName.toLowerCase()) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions().addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions().addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
+            }
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions().addArguments("--headless");
+                driver = new EdgeDriver(edgeOptions);
+            }
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
+
+        return driver;
     }
 }
